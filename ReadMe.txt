52b90ea50401299fd98e14cb410c20a2b17e3120
Mitch Congdon <mitchellcongdon@yahoo.com>
2017-03-30 10:16:20 -0400

    added more content
    Made it feel more alive.

---
+++ ReadMe.txt
@@ -0,0 +1,22 @@
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+			helloWorld.cpp project ReadMe.txt
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Hello, Upryzing here aka Mitchell Congdon. I have embarked on an adventure to learn
+
+a bit more about C++, and coding in general.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Feel free to propose changes and give advice.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This particular program is just a helloWorld console application that pulls from a
+
+class and prints to the console. 
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Upryzing 3/26/2017
---
+++ helloworld.cpp
@@ -0,0 +1,50 @@
+// Win32Project1.cpp : Defines the entry point for the console application.
+//
+
+/*
+Code written by Mitchell Congdon
+03/26/2017
+*/
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class WordsNStuff {
+public://makes it so you can access this code from int main or anwhere really
+	string name;
+	string greeting;
+	void say1() {
+		cout << "Look, I am a hello world program and here I am saying HELLO!!" << endl;
+	}
+	void say2() {
+		cout << name << ", I'm saying hello here..." << endl;
+	}
+	void say3() {
+		cout << "anyway, " << name << ", my name is Jerald, and I am a Hello world program.\nYou greeted me with " << greeting << "\nMy purpose is as simple as my being...\nHello World." << endl;
+	}
+};
+
+int main()
+{
+	WordsNStuff blab;
+	cout << "Enter your name:";
+		cin >> blab.name;
+		blab.say1();
+		system("pause");
+		blab.say2();
+		cin >> blab.greeting;
+		blab.say3();
+		
+	/*next up make more stuff.
+	private class variables etc.
+	look up how to get user input.
+	look up how to make functions.
+	decide upon a simple program to write,
+	write it and load it up to gitHub.
+	*/
+	system("pause");
+	return 0;
+}
+
